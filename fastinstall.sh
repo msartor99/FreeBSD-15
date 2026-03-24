18becd87f018e6b3757212fbbeac9310069423cf
msartor99 <148772297+msartor99@users.noreply.github.com>
2026-03-24 09:32:51 +0100

    Update fastinstall.sh to configure network interface
    New version, and some parameters added like promisc at ifconfig

--- fastinstall.sh
+++ fastinstall.sh
@@ -19,6 +19,7 @@ cd /tmp/fb15_assets
 cp "$DRIVER_NAME" "$MODULE_DIR/"
 chmod 555 "$MODULE_DIR/$DRIVER_NAME"
 
+
 # 2. Chargement immediat
 echo "Chargement du module..."
 kldload -v "$MODULE_DIR/$DRIVER_NAME"
@@ -26,14 +27,23 @@ kldload -v "$MODULE_DIR/$DRIVER_NAME"
 # 3. Configuration de la persistance au boot
 echo "Configuration du demarrage..."
 
+echo 'hw.pci.enable_aspm="0"' >> /boot/loader.conf
+echo 'hw.aq.msix_disable="1"' >> /boot/loader.conf
+echo 'hw.aq.num_queues="1"' >> /boot/loader.conf
+echo 'dev.aq.0.eee_enable="0"' >> /boot/loader.conf
+echo 'dev.aq.X.iflib.override_nrxqs="8"' >> /boot/loader.conf
+echo 'dev.aq.X.iflib.override_ntxqs=8"' >> /boot/loader.conf
+echo 'hw.dmar.enable="0"' >> /boot/loader.conf
+
 # Chargement auto du driver
 if ! grep -q "if_atlantic_load" /boot/loader.conf; then
     echo 'if_atlantic_load="YES"' >> /boot/loader.conf
+    sysrc kld_list+=if_atlantic
 fi
 
 # Activation reseau DHCP
 if ! grep -q "ifconfig_aq0" /etc/rc.conf; then
-    echo 'ifconfig_aq0="DHCP"' >> /etc/rc.conf
+    echo 'ifconfig_aq0="DHCP promisc"' >> /etc/rc.conf
 fi
 
 echo "Termine ! L'interface aq0 devrait etre active."
